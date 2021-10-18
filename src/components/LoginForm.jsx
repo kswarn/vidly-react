172bcbbcae719de6d15c3811d2db9304ade46176
kswarn <swarnakadagadkai1@gmail.com>
2021-10-18 22:19:05 +0530

    replacing the internal state of input element with the login form component state

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,16 +1,21 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState } from "react";
 
 const LoginForm = () => {
-  const usernameRef = useRef(null);
-
+  const [account, setAccount] = useState({
+    username: "",
+    password: "",
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("hey");
   };
 
-  useEffect(() => {
-    usernameRef.current.focus();
-  });
+  const handleInputChange = ({ target: input }) => {
+    const accountCopy = { ...account };
+    accountCopy[input.id] = input.value;
+    setAccount(accountCopy);
+  };
+
   return (
     <div>
       <h1>Login</h1>
@@ -18,8 +23,9 @@ const LoginForm = () => {
         <div className="form-group">
           <label htmlFor="username">Username</label>
           <input
-            // autoFocus={true}
-            ref={usernameRef}
+            autoFocus={true}
+            value={account.username}
+            onChange={handleInputChange}
             type="text"
             id="username"
             className="form-control"
@@ -27,7 +33,13 @@ const LoginForm = () => {
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input id="password" type="text" className="form-control"></input>
+          <input
+            id="password"
+            value={account.password}
+            onChange={handleInputChange}
+            type="text"
+            className="form-control"
+          ></input>
         </div>
         <button className="btn btn-primary">Login</button>
       </form>
