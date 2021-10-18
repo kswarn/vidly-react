8ca8ae0c586676e0bec5f74f0c16205f234c39ce
kswarn <swarnakadagadkai1@gmail.com>
2021-10-18 22:05:29 +0530

    implemented useRef hook in login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 const LoginForm = () => {
+  const usernameRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hey");
+  };
+
+  useEffect(() => {
+    usernameRef.current.focus();
+  });
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" className="form-control"></input>
+          <input
+            // autoFocus={true}
+            ref={usernameRef}
+            type="text"
+            id="username"
+            className="form-control"
+          ></input>
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
